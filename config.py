d0eb6ce09dd63f4289e1e944129e1f0aa5581bee
Mathews Jumba <ahmadashraf159@gmail.com>
2019-12-01 12:08:52 +0300

    this commit will add all the files for the Blogging_IP application

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+class Config:
+
+    QUOTES_API = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = os.environ.get('45454667ffhgfgf')
+    SECRET_KEY = os.urandom(32)
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adminpb:root@localhost/pb'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+class ProdConfig(Config):
+     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") 
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adminpb:root@localhost/pb_test'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adminpb:root@localhost/pb'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
