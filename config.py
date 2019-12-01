eb2d47db8624d3ca438edaf1191c3ee11e600216
Mathews Jumba <ahmadashraf159@gmail.com>
2019-12-01 16:04:29 +0300

    this commit will change config file configurations for production

--- config.py
+++ config.py
@@ -2,49 +2,31 @@ import os
 
 class Config:
 
-    '''
-    General configuration parent class
-    '''
     QUOTES_API = 'http://quotes.stormconsultancy.co.uk/random.json'
-    SECRET_KEY = '45454667ffhgfgf'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    # simple mde  configurations
+    SECRET_KEY = os.environ.get('45454667ffhgfgf')
+    SECRET_KEY = os.urandom(32)
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adminpb:root@localhost/pb'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
-
-
-
 class ProdConfig(Config):
-    '''
-    Pruduction configuration child class
+     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") 
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
-    '''
-    Testing configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings 
-    '''
-    pass
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adminpb:root@localhost/pb_test'
 
 class DevConfig(Config):
-    '''
-    Development configuration child class
-    
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    SQLALCHEMY_DATABASE_URI=''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adminpb:root@localhost/pb'
     DEBUG = True
 
-
 config_options = {
-    'development': DevConfig,
-    'production': ProdConfig,
-    'test': TestConfig
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
 }
